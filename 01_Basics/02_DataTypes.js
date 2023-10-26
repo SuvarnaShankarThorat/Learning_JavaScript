02cf91e1267152e8a99d71d8024cc2b2f56e2500
Suvarna Shankar Thorat <147941489+SuvarnaShankarThorat@users.noreply.github.com>
2023-10-26 16:58:39 +0000

    DataTypes and Conversions disccusions

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"Use strict"; // Treat all js code as a newer version
+
+//alert("Hi")// We are using node js not a broswers
+
+let myName="Suvarna"
+let age=28
+let isSoftwareEngineer=true
+let state;
+let city=null;
+
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof city);
+// number => 1233 
+// string => "" 
+// boolean => true/ false
+// undefined => undefined
+// null => null 
---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score="33sasas";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNum=Number(score);
+console.log(typeof ValueInNum);
+console.log(ValueInNum);
+
+// Conversion points
+//"33" => 33 convert to numbers
+//"33abc"=> NaN => Not a Number
+//true=>1; false=>0
+
+//let isLoggedIn=1;
+let isLoggedIn="SUvarna";
+let checkLoggedIn=Boolean(isLoggedIn);
+console.log(checkLoggedIn);
+console.log(typeof checkLoggedIn);
+
+// Boolean 
+//1=> true; 0=>false
+//"" => true
+//"Suvarna" => NaN
+
+
+
